83a79b0cc496ae348542c5772076a18aec543fe5
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-06-06 11:15:04 +0700

    pkg: add server package
    Add server package, which has responsibility to run the internal http
    server and handle event from broker.

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/valve v0.0.0-20170920024740-9e45288364f4
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.0
---
+++ pkg/server/server.go
@@ -0,0 +1,172 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/valve"
+	"go.uber.org/zap"
+
+	"github.com/bizflycloud/bizfly-backup/pkg/broker"
+)
+
+// Server defines parameters for running BizFly Backup HTTP server.
+type Server struct {
+	Addr        string
+	router      *chi.Mux
+	b           broker.Broker
+	useUnixSock bool
+
+	// signal chan use for testing.
+	testSignalCh chan os.Signal
+
+	logger *zap.Logger
+}
+
+// New creates new server instance.
+func New(opts ...Option) (*Server, error) {
+	s := &Server{}
+	for _, opt := range opts {
+		if err := opt(s); err != nil {
+			return nil, err
+		}
+	}
+
+	s.router = chi.NewRouter()
+
+	if s.logger == nil {
+		l, err := zap.NewDevelopment()
+		if err != nil {
+			return nil, err
+		}
+		s.logger = l
+	}
+
+	s.setupRoutes()
+	s.useUnixSock = strings.HasPrefix(s.Addr, "unix://")
+	s.Addr = strings.TrimPrefix(s.Addr, "unix://")
+
+	return s, nil
+}
+
+func (s *Server) setupRoutes() {
+	s.router.Route("/backups", func(r chi.Router) {
+		r.Get("/", s.ListBackup)
+		r.Post("/", s.Backup)
+		r.Post("/restore", s.Restore)
+	})
+
+	s.router.Route("/cron", func(r chi.Router) {
+		s.router.Patch("/{id}", s.UpdateCron)
+	})
+
+	s.router.Route("/upgrade", func(r chi.Router) {
+		s.router.Post("/", s.UpgradeAgent)
+	})
+}
+
+func (s *Server) handleBrokerEvent(e broker.Event) error {
+	var msg broker.Message
+	if err := json.Unmarshal(e.Payload, &msg); err != nil {
+		return err
+	}
+	switch msg.EventType {
+	case broker.BackupManual, broker.RestoreManual, broker.ConfigUpdate, broker.AgentUpgrade:
+	// TODO: handle event
+	default:
+		return fmt.Errorf("Event %s: %w", msg.EventType, broker.ErrUnknownEventType)
+	}
+	return nil
+}
+
+func (s *Server) Backup(w http.ResponseWriter, r *http.Request)       {}
+func (s *Server) ListBackup(w http.ResponseWriter, r *http.Request)   {}
+func (s *Server) Restore(w http.ResponseWriter, r *http.Request)      {}
+func (s *Server) UpdateCron(w http.ResponseWriter, r *http.Request)   {}
+func (s *Server) UpgradeAgent(w http.ResponseWriter, r *http.Request) {}
+
+func (s *Server) Run() error {
+	// Graceful valve shut-off package to manage code preemption and shutdown signaling.
+	valv := valve.New()
+	baseCtx := valv.Context()
+
+	go func(ctx context.Context) {
+		for {
+			<-time.After(1 * time.Second)
+
+			func() {
+				if err := valve.Lever(ctx).Open(); err != nil {
+					s.logger.Error("failed to open valve")
+					return
+				}
+				defer valve.Lever(ctx).Close()
+
+				// signal control.
+				select {
+				case <-valve.Lever(ctx).Stop():
+					s.logger.Debug("valve is closed")
+					return
+
+				case <-ctx.Done():
+					s.logger.Debug("context is cancelled")
+					return
+				default:
+				}
+			}()
+		}
+	}(baseCtx)
+
+	srv := http.Server{Handler: chi.ServerBaseContext(baseCtx, s.router)}
+
+	c := make(chan os.Signal, 1)
+	if s.testSignalCh != nil {
+		c = s.testSignalCh
+	}
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		<-c
+		// signal is a ^C, handle it
+		s.logger.Info("shutting down...")
+
+		// first valv
+		if err := valv.Shutdown(20 * time.Second); err != nil {
+			s.logger.Error("failed to shutdown valv")
+		}
+
+		// create context with timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+
+		// start http shutdown
+		if err := srv.Shutdown(ctx); err != nil {
+			s.logger.Error("failed to shutdown http server")
+		}
+
+		// verify, in worst case call cancel via defer
+		select {
+		case <-time.After(21 * time.Second):
+			s.logger.Error("not all connections done")
+		case <-ctx.Done():
+		}
+	}()
+
+	if s.useUnixSock {
+		unixListener, err := net.Listen("unix", s.Addr)
+		if err != nil {
+			return err
+		}
+		return srv.Serve(unixListener)
+	}
+
+	srv.Addr = s.Addr
+	return srv.ListenAndServe()
+}

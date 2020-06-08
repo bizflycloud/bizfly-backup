967add370b8c57efa9ed57dbcfd166642e6bc594
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-06-08 09:16:59 +0700

    pkg/broker/mqtt: return error for empty url

--- pkg/broker/mqtt/option.go
+++ pkg/broker/mqtt/option.go
@@ -1,12 +1,18 @@
 package mqtt
 
-import "net/url"
+import (
+	"errors"
+	"net/url"
+)
 
 type Option func(m *mqttBroker) error
 
 // WithURL returns an Option which set the broker url.
 func WithURL(u string) Option {
 	return func(m *mqttBroker) error {
+		if u == "" {
+			return errors.New("empty broker url")
+		}
 		uri, err := url.Parse(u)
 		if err != nil {
 			return err

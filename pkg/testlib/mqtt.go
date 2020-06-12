de08e564da637ff26f5c43b089d9a8f2e8cb2561
greatbn <great_bn@yahoo.com>
2020-06-12 14:12:48 +0700

    use fluux.io mqtt server

--- pkg/testlib/mqtt.go
+++ pkg/testlib/mqtt.go
@@ -4,7 +4,7 @@ import "os"
 
 func MqttUrl() string {
 	if os.Getenv("GITHUB_ACTION") != "" {
-		return "mqtt://test.mosquitto.org:1883"
+		return "mqtt://mqtt.fluux.io:1883"
 	}
 	return "mqtt://foo:bar@localhost:1883"
 }

5d07073280a1f6051ec803b0696ee40f9b53bff3
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-07-04 10:43:19 +0700

    pkg/backupapi: add Client

---
+++ pkg/backupapi/client_test.go
@@ -0,0 +1,95 @@
+package backupapi
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	client *Client
+	mux    *http.ServeMux
+	server *httptest.Server
+)
+
+func setUp() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	client, _ = NewClient()
+	serverURL, _ := url.Parse(server.URL)
+	client.ServerURL = serverURL
+}
+
+func tearDown() {
+	server.Close()
+}
+
+func TestClientOptions(t *testing.T) {
+	tests := []struct {
+		name       string
+		opt        ClientOption
+		wantErr    bool
+		assertFunc func(c *Client) bool
+	}{
+		{"valid http client", WithHTTPClient(http.DefaultClient), false, func(c *Client) bool { return c.client == http.DefaultClient }},
+		{"nil http client", WithHTTPClient(nil), true, nil},
+		{"valid server url", WithServerURL("https://foo.bar/api/v1"), false, func(c *Client) bool { return c.ServerURL.Host == "foo.bar" && c.ServerURL.Path == "/api/v1" }},
+		{"invalid server url", WithServerURL("https://:foo.bar/api/v1"), true, nil},
+		{"access key", WithAccessKey("access_key"), false, func(c *Client) bool { return c.accessKey == "access_key" }},
+		{"secret key", WithSecretKey("secret_key"), false, func(c *Client) bool { return c.secretKey == "secret_key" }},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			c, err := NewClient(tc.opt)
+			requireFunc := require.NoError
+			if tc.wantErr {
+				requireFunc = require.Error
+			}
+			requireFunc(t, err)
+			if tc.assertFunc != nil {
+				assert.True(t, tc.assertFunc(c))
+			}
+		})
+	}
+}
+
+func TestDo(t *testing.T) {
+	setUp()
+	defer tearDown()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+		assert.Equal(t, r.Header.Get("User-Agent"), "bizfly-backup-client")
+
+		_, _ = w.Write([]byte("foo"))
+	})
+
+	req, _ := client.NewRequest("GET", "/", nil)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		t.Fatalf("Do(): %v", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Do(): %v", err)
+	}
+
+	res := string(body)
+	expected := "foo"
+	if !reflect.DeepEqual(res, expected) {
+		t.Fatalf("Expected %v - Got %v", expected, res)
+	}
+}

cf189e7f336d5d041deddf9b47675f2f6c0a820d
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-08-07 17:07:37 +0700

    all: using dockertest
    To prevent Github Action flaky
    
    Fixes BK-111

--- pkg/broker/mqtt/mqtt_test.go
+++ pkg/broker/mqtt/mqtt_test.go
@@ -1,27 +1,25 @@
 package mqtt
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/bizflycloud/bizfly-backup/pkg/broker"
-	"github.com/bizflycloud/bizfly-backup/pkg/testlib"
 )
 
-func TestMQTT(t *testing.T) {
-	if os.Getenv("EXCLUDE_MQTT") != "" {
-		return
-	}
+var (
+	sub     broker.Broker
+	mqttURL string
+)
+
+func testMQTT(t *testing.T) {
 	topics := []string{"1", "2", "3"}
 	done := make(chan struct{}, 1)
-	mqttUrl := testlib.MqttUrl()
-	sub, err := NewBroker(WithURL(mqttUrl), WithClientID("sub"))
-	require.NoError(t, err)
-	require.NotNil(t, sub)
-	assert.NoError(t, sub.Connect())
 
 	go func() {
 		count := 0
@@ -35,7 +33,7 @@ func TestMQTT(t *testing.T) {
 		}))
 	}()
 
-	pub, err := NewBroker(WithURL(mqttUrl), WithClientID("pub"))
+	pub, err := NewBroker(WithURL(mqttURL), WithClientID("pub"))
 	require.NoError(t, err)
 	require.NotNil(t, pub)
 	assert.NoError(t, pub.Connect())
@@ -45,4 +43,49 @@ func TestMQTT(t *testing.T) {
 	}
 
 	<-done
+}
+
+func TestMQTT(t *testing.T) {
+	if os.Getenv("EXCLUDE_MQTT") != "" {
+		return
+	}
+
+	runWithVerneMQDockerImage(
+		"vernemq/vernemq",
+		"latest-alpine",
+		[]string{"DOCKER_VERNEMQ_USER_foo=bar", "DOCKER_VERNEMQ_ACCEPT_EULA=yes"},
+		testMQTT,
+		t,
+	)
+}
+
+func runWithVerneMQDockerImage(repo, tag string, env []string, testFunc func(t *testing.T), t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		t.Fatalf("Could not connect to docker: %s", err)
+	}
+	resource, err := pool.Run(repo, tag, env)
+	if err != nil {
+		t.Fatalf("Could not start resource: %s", err)
+	}
+
+	defer func() {
+		if err := pool.Purge(resource); err != nil {
+			t.Fatalf("Could not purge resource: %s", err)
+		}
+	}()
+
+	mqttURL = fmt.Sprintf("mqtt://foo:bar@%s", resource.GetHostPort("1883/tcp"))
+	if err := pool.Retry(func() error {
+		var err error
+		sub, err = NewBroker(WithURL(mqttURL), WithClientID("sub"))
+		if err != nil {
+			return err
+		}
+		return sub.Connect()
+	}); err != nil {
+		t.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	testFunc(t)
 }

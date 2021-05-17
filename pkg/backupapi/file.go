47acea370cbf194eff6629b17add8793473bddad
Nguyen Dac Toan <toannd@vccloud.vn>
2021-05-17 15:46:15 +0700

    del comment

--- pkg/backupapi/file.go
+++ pkg/backupapi/file.go
@@ -189,9 +189,6 @@ func (c *Client) UploadFile(recoveryPointID string, backupDir string, fi File, v
 			return err
 		}
 
-		// log.Println("SAVE CHUNK KEY", chunkReq.HexSha256)
-		// log.Printf("chunk info %d\t%d\t%016x\t%032x\n", chunk.Start, chunk.Length, chunk.Cut, hash)
-
 		exist, err := volume.HeadObject(chunkResp.PresignedURL.Head)
 		if err != nil {
 			return err

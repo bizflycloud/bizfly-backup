55a6a3f76b5b0b8c28ac37f2aff7d0e3625a3470
Nguyen Dac Toan <toannd@vccloud.vn>
2021-05-25 16:31:16 +0700

    add field volume struct

--- pkg/backupapi/volume.go
+++ pkg/backupapi/volume.go
@@ -8,4 +8,8 @@ type Volume struct {
 	StorageBucket string `json:"storage_bucket"`
 	VolumeType    string `json:"volume_type"`
 	SecretRef     string `json:"secret_ref"`
+	CreatedAt     string `json:"created_at"`
+	UpdatedAt     string `json:"updated_at"`
+	Deleted       bool   `json:"deleted"`
+	EncryptionKey string `json:"encryption_key"`
 }

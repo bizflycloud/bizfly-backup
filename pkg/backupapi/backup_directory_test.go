2c9c058d74be2a11f5f0c715c969b37661a2415f
Nguyen Dac Toan <toannd@vccloud.vn>
2021-05-26 17:30:51 +0700

    test client get backup dir

--- pkg/backupapi/backup_directory_test.go
+++ pkg/backupapi/backup_directory_test.go
@@ -1,9 +1,13 @@
 package backupapi
 
 import (
+	"encoding/json"
+	"net/http"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestClient_backupDirectoryPath(t *testing.T) {
@@ -22,4 +26,31 @@ func TestClient_backupDirectoryActionPath(t *testing.T) {
 	id := "id"
 	bdap := client.backupDirectoryActionPath(id)
 	assert.Equal(t, "/agent/backup-directories/id/action", bdap)
+}
+
+func TestClient_GetBackupDirectory(t *testing.T) {
+	setUp()
+	defer tearDown()
+
+	id := "id"
+	bdp := client.backupDirectoryPath(id)
+
+	mux.HandleFunc(path.Join("/api/v1", bdp), func(w http.ResponseWriter, r *http.Request) {
+		resp := &BackupDirectory{
+			ID:          "action-id",
+			Name:        "name",
+			Description: "description",
+			Path:        "path",
+			Quota:       1,
+			Size:        1,
+			MachineID:   "machine-id",
+			TenantID:    "tenant-id",
+		}
+
+		assert.NoError(t, json.NewEncoder(w).Encode(resp))
+	})
+
+	rps, err := client.GetBackupDirectory(id)
+	require.NoError(t, err)
+	assert.NotEmpty(t, rps.ID)
 }

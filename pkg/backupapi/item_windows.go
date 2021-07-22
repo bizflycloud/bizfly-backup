d096530a755dc825d8b6e66da756349d48631b2d
dactoankmapydev <nguyendactoankma@gmail.com>
2021-07-22 16:50:18 +0700

    set type uint32 uid,gid item in windows

--- pkg/backupapi/item_windows.go
+++ pkg/backupapi/item_windows.go
@@ -13,8 +13,8 @@ func ItemLocal(fi fs.FileInfo) (time.Time, time.Time, time.Time, uint32, uint32)
 		ctimeLocal = time.Unix(0, stat.LastAccessTime.Nanoseconds()).UTC()
 		mtimeLocal = time.Unix(0, stat.LastAccessTime.Nanoseconds()).UTC()
 	}
-	uid := 0
-	gid := 0
+	uid := uint32(0)
+	gid := uint32(0)
 
 	return atimeLocal, ctimeLocal, mtimeLocal, uid, gid
 }

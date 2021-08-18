78e41c8f4991ee7e4cbe19f6a7c5ea3095a61e68
dactoankmapydev <nguyendactoankma@gmail.com>
2021-08-18 15:21:31 +0700

    fix get file size windows

--- pkg/support/item_windows.go
+++ pkg/support/item_windows.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-func ItemLocal(fi fs.FileInfo) (time.Time, time.Time, time.Time, uint32, uint32, int64) {
+func ItemLocal(fi fs.FileInfo) (time.Time, time.Time, time.Time, uint32, uint32, uint32) {
 	var atimeLocal, ctimeLocal, mtimeLocal time.Time
-	var size int64
+	var size uint32
 	if stat, ok := fi.Sys().(*syscall.Win32FileAttributeData); ok {
 		atimeLocal = time.Unix(0, stat.LastAccessTime.Nanoseconds()).UTC()
 		ctimeLocal = time.Unix(0, stat.LastWriteTime.Nanoseconds()).UTC()
 		mtimeLocal = time.Unix(0, stat.LastWriteTime.Nanoseconds()).UTC()
-		size = stat.Size
+		size = stat.FileSizeHigh
 	}
 	uid := uint32(0)
 	gid := uint32(0)

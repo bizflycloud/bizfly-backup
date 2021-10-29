1fea77cb53c965ff813869d0a214f236f275f3e0
Nguyen Dac Toan <toannd@vccloud.vn>
2021-10-29 09:19:38 +0700

    add command remove old cache dir after max time

---
+++ cmd/cleanup_cache.go
@@ -0,0 +1,61 @@
+// This file is part of bizfly-backup
+//
+// Copyright (C) 2020  BizFly Cloud
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/bizflycloud/bizfly-backup/pkg/cache"
+	"github.com/spf13/cobra"
+)
+
+const (
+	cacheDir = ".cache"
+)
+
+var cleanupCacheCmd = &cobra.Command{
+	Use:   "cleanup-cache",
+	Short: "Remove old cache directories",
+	Run: func(cmd *cobra.Command, args []string) {
+		oldCacheDirs, err := cache.Old(cacheDir)
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("%d old cache dirs found \n", len(oldCacheDirs))
+
+		// nothing more to do if no old cache dirs could be found
+		if len(oldCacheDirs) != 0 {
+			for _, item := range oldCacheDirs {
+				dir := filepath.Join(cacheDir, item.Name())
+				err = os.RemoveAll(dir)
+				if err != nil {
+					logger.Error(err.Error())
+					os.Exit(1)
+				}
+				fmt.Printf("removing old cache dirs %s \n", dir)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cleanupCacheCmd)
+}
--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -2,15 +2,21 @@ package cache
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
+	"time"
 )
 
 const (
 	dirMode  = 0700
 	tempPath = "tmp"
+
+	// maxCacheAge is the default age (30 days) after which cache
+	// directories are considered old
+	maxCacheAge = 30 * 24 * time.Hour
 )
 
 type Repository struct {
@@ -129,4 +135,71 @@ func (r *Repository) SaveChunk(chunk *Chunk) error {
 		return err
 	}
 	return nil
+}
+
+// listCacheDirs returns the list of cache directories.
+func listCacheDirs(cacheDir string) ([]os.FileInfo, error) {
+	f, err := os.Open(cacheDir)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	entries, err := f.Readdir(-1)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	result := make([]os.FileInfo, 0, len(entries))
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+
+		result = append(result, entry)
+	}
+
+	return result, nil
+}
+
+// olderThan returns the list of cache directories older than max.
+func olderThan(cacheDir string) ([]os.FileInfo, error) {
+	entries, err := listCacheDirs(cacheDir)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var oldCacheDirs []os.FileInfo
+	for _, fi := range entries {
+		if !isOld(fi.ModTime()) {
+			continue
+		}
+		oldCacheDirs = append(oldCacheDirs, fi)
+	}
+
+	return oldCacheDirs, nil
+}
+
+// Old returns a list of cache directories with a modification time of more
+// than 30 days ago.
+func Old(basedir string) ([]os.FileInfo, error) {
+	return olderThan(basedir)
+}
+
+// isOld returns true if the timestamp is considered old.
+func isOld(t time.Time) bool {
+	oldest := time.Now().Add(-maxCacheAge)
+	return t.Before(oldest)
 }

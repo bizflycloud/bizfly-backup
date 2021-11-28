3251e39b27423519d4613b95584abc8fb1e376c6
ngocvinhvu <69155752+ngocvinhvu@users.noreply.github.com>
2021-11-28 08:12:54 +0700

    Add-recovery-point-id-when-show-progress (#131)
    Co-authored-by: vinhvn <vinhvn@bizflycloud.vn>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1203,14 +1203,15 @@ func (s *Server) newUploadProgress(recoveryPointID string, todo progress.Stat) *
 			strItemsTodo := strconv.FormatUint(itemsTodo, 10)
 
 			s.notifyMsgProgress(recoveryPointID, map[string]string{
-				"duration":     formatDuration(d),
-				"percent":      formatPercent(stat.Bytes, todo.Bytes),
-				"speed":        formatBytes(bps),
-				"total":        fmt.Sprintf("%s/%s", formatBytes(stat.Bytes), formatBytes(todo.Bytes)),
-				"push_storage": formatBytes(stat.Storage),
-				"items":        fmt.Sprintf("%s/%s", strItemsDone, strItemsTodo),
-				"erros":        strconv.FormatBool(stat.Errors),
-				"eta":          formatSeconds(eta),
+				"duration":          formatDuration(d),
+				"percent":           formatPercent(stat.Bytes, todo.Bytes),
+				"speed":             formatBytes(bps),
+				"total":             fmt.Sprintf("%s/%s", formatBytes(stat.Bytes), formatBytes(todo.Bytes)),
+				"push_storage":      formatBytes(stat.Storage),
+				"items":             fmt.Sprintf("%s/%s", strItemsDone, strItemsTodo),
+				"erros":             strconv.FormatBool(stat.Errors),
+				"eta":               formatSeconds(eta),
+				"recovery_point_id": recoveryPointID,
 			})
 		}
 	}
@@ -1248,14 +1249,15 @@ func (s *Server) newDownloadProgress(recoveryPointID string, todo progress.Stat)
 			strItemsTodo := strconv.FormatUint(itemsTodo, 10)
 
 			s.notifyMsgProgress(recoveryPointID, map[string]string{
-				"duration":     formatDuration(d),
-				"percent":      formatPercent(stat.Bytes, todo.Bytes),
-				"speed":        formatBytes(bps),
-				"total":        fmt.Sprintf("%s/%s", formatBytes(stat.Bytes), formatBytes(todo.Bytes)),
-				"pull_storage": formatBytes(stat.Storage),
-				"items":        fmt.Sprintf("%s/%s", strItemsDone, strItemsTodo),
-				"erros":        strconv.FormatBool(stat.Errors),
-				"eta":          formatSeconds(eta),
+				"duration":          formatDuration(d),
+				"percent":           formatPercent(stat.Bytes, todo.Bytes),
+				"speed":             formatBytes(bps),
+				"total":             fmt.Sprintf("%s/%s", formatBytes(stat.Bytes), formatBytes(todo.Bytes)),
+				"pull_storage":      formatBytes(stat.Storage),
+				"items":             fmt.Sprintf("%s/%s", strItemsDone, strItemsTodo),
+				"erros":             strconv.FormatBool(stat.Errors),
+				"eta":               formatSeconds(eta),
+				"recovery_point_id": recoveryPointID,
 			})
 		}
 	}

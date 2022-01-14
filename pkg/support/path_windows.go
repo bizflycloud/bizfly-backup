b7807a1c9b216517a7923d40fba6cf50c78448c2
dactoankmapydev <nguyendactoankma@gmail.com>
2022-01-14 10:11:01 +0700

    edit path log and cache on windows

--- pkg/support/path_windows.go
+++ pkg/support/path_windows.go
@@ -5,34 +5,12 @@ import (
 	"strings"
 )
 
-const (
-	LOG_ERROR_PATH = "C:\\Program Files\\bizfly-backup\\log\\error.log"
-	LOG_DEBUG_PATH = "C:\\Program Files\\bizfly-backup\\log\\debug.log"
-	LOG_INFO_PATH  = "C:\\Program Files\\bizfly-backup\\log\\info.log"
-
-	CACHE_PATH = "C:\\Program Files\\bizfly-backup\\lib\\.cache"
-)
-
 func CheckPath() (string, string, string, string, error) {
-	var logErrorPath, logDebugPath, logInfoPath, cachePath string
-	user, err := user.Current()
-	if err != nil {
-		return "", "", "", "", err
-	}
+	logErrorPath := "C:\\Program Files\\bizfly-backup\\log\\error.log"
+	logDebugPath := "C:\\Program Files\\bizfly-backup\\log\\debug.log"
+	logInfoPath := "C:\\Program Files\\bizfly-backup\\log\\info.log"
 
-	if strings.Contains(user.Username, "Administrator") {
-		logErrorPath = "C:\\Program Files\\bizfly-backup\\log\\error.log"
-		logDebugPath = "C:\\Program Files\\bizfly-backup\\log\\debug.log"
-		logInfoPath = "C:\\Program Files\\bizfly-backup\\log\\info.log"
-
-		cachePath = "C:\\Program Files\\bizfly-backup\\lib\\.cache"
-	} else {
-		logErrorPath = user.HomeDir + "/var/log/bizfly-backup/error.log"
-		logDebugPath = user.HomeDir + "/var/log/bizfly-backup/debug.log"
-		logInfoPath = user.HomeDir + "/var/log/bizfly-backup/info.log"
-
-		cachePath = ".cache"
-	}
+	cachePath := "C:\\Program Files\\bizfly-backup\\lib\\.cache"
 
 	return logErrorPath, logDebugPath, logInfoPath, cachePath, nil
 }

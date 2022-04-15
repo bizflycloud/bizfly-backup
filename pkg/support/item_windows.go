9b2fd894c876465117e3c9afa731b8e00d9d41d9
Tris Duong <70458716+trisduong@users.noreply.github.com>
2022-04-15 17:02:32 +0700

    fix/fix-file-size-on-windows-2008 (#162)
    Co-authored-by: tridv <tridv@bizflycloud.vn>

--- pkg/support/item_windows.go
+++ pkg/support/item_windows.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-func ItemLocal(fi fs.FileInfo) (time.Time, time.Time, time.Time, uint32, uint32, uint32) {
+func ItemLocal(fi fs.FileInfo) (time.Time, time.Time, time.Time, uint32, uint32, int64) {
 	var atimeLocal, ctimeLocal, mtimeLocal time.Time
-	var size uint32
+	var size int64
 	if stat, ok := fi.Sys().(*syscall.Win32FileAttributeData); ok {
 		atimeLocal = time.Unix(0, stat.LastAccessTime.Nanoseconds()).UTC()
 		ctimeLocal = time.Unix(0, stat.LastWriteTime.Nanoseconds()).UTC()
 		mtimeLocal = time.Unix(0, stat.LastWriteTime.Nanoseconds()).UTC()
-		size = stat.FileSizeHigh
+		size = fi.Size()
 	}
 	uid := uint32(0)
 	gid := uint32(0)
